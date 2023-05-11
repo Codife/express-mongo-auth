b1e343451eee8b980a21be1448e687481cc00cd4
hunainjamil <hunainjamil3@gmail.com>
2023-05-11 16:14:32 +0500

    token verification and auth apis

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+require("dotenv").config();
 var createError = require("http-errors");
 var express = require("express");
 var path = require("path");
@@ -6,7 +7,7 @@ var logger = require("morgan");
 var mongoose = require("mongoose");
 
 var indexRouter = require("./routes/index");
-var usersRouter = require("./routes/users");
+var authRouter = require("./routes/auth");
 
 var app = express();
 
@@ -14,10 +15,9 @@ app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, "public")));
 
 app.use("/", indexRouter);
-app.use("/users", usersRouter);
+app.use("/auth", authRouter);
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
@@ -30,19 +30,18 @@ app.use(function (err, req, res, next) {
   res.locals.message = err.message;
   res.locals.error = req.app.get("env") === "development" ? err : {};
 
-  // render the error page
-  res.status(err.status || 500);
-  res.render("error");
+  // send JSON response
+  res.status(err.status || 500).json({
+    message: err.message || "Internal Server Error",
+    error: err,
+  });
 });
 
 mongoose
-  .connect(
-    "mongodb+srv://hunain:1234@testcluster1.v1h6c.mongodb.net/pc_management?retryWrites=true&w=majority",
-    {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    }
-  )
+  .connect(process.env.MONGO_DB_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
   .then(() => {
     console.log("Connected to database");
   })
@@ -50,6 +49,6 @@ mongoose
     console.error("Error connecting to database", error);
   });
 
-app.listen(4000, () => {
-  console.log(`Server is listening on port 4000`);
+app.listen(process.env.PORT || 4000, () => {
+  console.log(`Server is listening on port ${process.env.PORT || 4000}`);
 });
--- models/UserSchema.js
+++ models/UserSchema.js
@@ -1,21 +1,26 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
 const userSchema = new mongoose.Schema({
   name: {
     type: String,
-    required: true
+    required: true,
   },
   email: {
     type: String,
     required: true,
-    unique: true
+    unique: true,
+  },
+  password: {
+    type: String,
+    required: true,
   },
   userType: {
     type: String,
-    required: true
-  }
+    required: true,
+    enum: ["Admin", "User", "Lead"],
+  },
 });
 
-const User = mongoose.model('User', userSchema);
+const User = mongoose.model("User", userSchema);
 
 module.exports = User;
